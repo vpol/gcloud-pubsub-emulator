b50bcc445bf477ce76634c176a6711c2988be202
Viktor Poluksht <vpoluksht@gmail.com>
2022-06-18 15:57:32 +0100

    perf: faster creation
    Signed-off-by: Viktor Poluksht <vpoluksht@gmail.com>

--- pubsubc/main.go
+++ pubsubc/main.go
@@ -12,26 +12,16 @@ import (
 	"time"
 )
 
-func create(ctx context.Context, project string, subscription string, topic string) error {
+func create(ctx context.Context, log zerolog.Logger, client *pubsub.Client, subscription string, topic string) error {
 
-	logger := log.With().Str("project", project).Str("subscription", subscription).Str("topic", topic).Logger()
-
-	client, err := pubsub.NewClient(ctx, project)
-	if err != nil {
-		return fmt.Errorf("unable to create client to project %q: %s", project, err)
-	}
-
-	defer func() {
-		client.Close()
-		<-time.After(3 * time.Second)
-	}()
+	logger := log.With().Str("subscription", subscription).Str("topic", topic).Logger()
 
 	logger.Trace().Msg("client connected")
 
 	t, err := client.CreateTopic(ctx, topic)
 	if err != nil {
 		if !strings.Contains(err.Error(), "AlreadyExists") {
-			return fmt.Errorf("unable to create topic %q for project %q: %s", topic, project, err)
+			return fmt.Errorf("unable to create topic %q: %w", topic, err)
 		}
 
 		t = client.Topic(topic)
@@ -41,7 +31,7 @@ func create(ctx context.Context, project string, subscription string, topic stri
 
 	_, err = client.CreateSubscription(ctx, subscription, pubsub.SubscriptionConfig{Topic: t})
 	if err != nil {
-		return fmt.Errorf("unable to create subscription %q on topic %q for project %q: %s", subscription, topic, project, err)
+		return fmt.Errorf("unable to create subscription %q on topic %q: %w", subscription, topic, err)
 	}
 
 	logger.Trace().Msg("subscription created")
@@ -88,12 +78,35 @@ func main() {
 		log.Fatal().Err(err).Msg("failed to decode file")
 	}
 
-	for _, v := range config.Subscriptions {
-		err := create(context.TODO(), v.Project, v.Subscription, v.Topic)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to process subscription")
+	ctx := context.TODO()
 
+	var connectors = make(map[string]*pubsub.Client)
+	defer func() {
+		// close them all
+		for _, c := range connectors {
+			_ = c.Close()
 		}
-	}
+		time.Sleep(3 * time.Second)
+	}()
 
+	for _, v := range config.Subscriptions {
+
+		c, ok := connectors[v.Project]
+		if !ok {
+			// if no connector for project - connect
+			c, err = pubsub.NewClient(ctx, v.Project)
+			if err != nil {
+				log.Panic().Err(err).Str("project", v.Project).Msg("unable to create client")
+			}
+			connectors[v.Project] = c
+		}
+
+		logger := log.With().Str("project", v.Project).Logger()
+
+		err := create(context.TODO(), logger, c, v.Subscription, v.Topic)
+		if err != nil {
+			logger.Fatal().Err(err).Msg("failed to process subscription")
+		}
+
+	}
 }
